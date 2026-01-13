4f39946e308fe61efa23b2eb89acd75e3e5a3641
Jonathan Oh <jonathanoh1994@gmail.com>
2026-01-13 11:19:55 -0500

    chore: bump version to 3.0.0a2 for Phase 3

--- ontos/__init__.py
+++ ontos/__init__.py
@@ -7,7 +7,7 @@ Package structure:
     ontos.ui   - I/O layer (CLI, output, prompts)
 """
 
-__version__ = "3.0.0a1"
+__version__ = "3.0.0a2"
 
 # Re-export commonly used items for convenience
 from ontos.core.context import SessionContext, FileOperation, PendingWrite
